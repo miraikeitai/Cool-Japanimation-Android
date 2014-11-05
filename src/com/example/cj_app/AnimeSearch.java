1336c29f57a6c5fe3a5eecf8cfe1e94e8b03070e
Kazuki Iwata <iwkz.mazinken@gmail.com>
2014-11-05 15:02:03 +0900

    マージ

---
+++ src/com/example/cj_app/AnimeSearch.java
@@ -0,0 +1,11 @@
+package com.example.cj_app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class AnimeSearch extends ActionBarActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_animesearch_main);
+	}
+}
