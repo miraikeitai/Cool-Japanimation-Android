9ae1ca68bfca50d520fb100ee7473a8f89e1ac06
Kazuki Iwata <iwkz.mazinken@gmail.com>
2014-11-07 15:33:06 +0900

    再マージ

--- src/com/example/cj_app/AnimeSearch.java
+++ src/com/example/cj_app/AnimeSearch.java
@@ -2,10 +2,17 @@ package com.example.cj_app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageButton;
 
 public class AnimeSearch extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_animesearch_main);
+		
+		ImageButton Sbt = (ImageButton)findViewById(R.id.AniSearchBT);
+		
+		
+		
+		
 	}
 }
