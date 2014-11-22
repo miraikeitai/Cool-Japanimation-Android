71a30e797e17c2f90fa2ad4cf14a9bebc07810b9
Kazuki Iwata <iwkz.mazinken@gmail.com>
2014-11-23 05:13:49 +0900

    アニメ検索：ハリボテ

--- src/com/example/cj_app/AnimeSearchResult.java
+++ src/com/example/cj_app/AnimeSearchResult.java
@@ -1,7 +1,9 @@
 package com.example.cj_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class AnimeSearchResult extends ActionBarActivity {
@@ -11,6 +13,18 @@ public class AnimeSearchResult extends ActionBarActivity {
 
 		ImageButton Sbt = (ImageButton)findViewById(R.id.AniSearchBT);
 
+		ImageButton Nbt = (ImageButton)findViewById(R.id.NarutoBT);
+
+		Nbt.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// インテントのインスタンス生成
+				Intent intent = new Intent(AnimeSearchResult.this, AnimeSearchDetail.class);
+				// 次画面のアクティビティ起動
+				startActivity(intent);
+				}
+		});
 
 
 
