2ee45d74fda5924df5f0d2779d9368a6c8ac352f
Kazuki Iwata <iwkz.mazinken@gmail.com>
2014-11-23 04:35:50 +0900

    検索部分のハリボテを作成

--- src/com/example/cj_app/AnimeSearch.java
+++ src/com/example/cj_app/AnimeSearch.java
@@ -1,18 +1,29 @@
 package com.example.cj_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class AnimeSearch extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_animesearch_main);
-		
+
 		ImageButton Sbt = (ImageButton)findViewById(R.id.AniSearchBT);
-		
-		
-		
-		
+
+		Sbt.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// インテントのインスタンス生成
+				Intent intent = new Intent(AnimeSearch.this, AnimeSearchResult.class);
+				// 次画面のアクティビティ起動
+				startActivity(intent);
+				}
+		});
+
+
 	}
 }
---
+++ src/com/example/cj_app/AnimeSearchResult.java
@@ -0,0 +1,18 @@
+package com.example.cj_app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageButton;
+
+public class AnimeSearchResult extends ActionBarActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_animesearch_result);
+
+		ImageButton Sbt = (ImageButton)findViewById(R.id.AniSearchBT);
+
+
+
+
+	}
+}
