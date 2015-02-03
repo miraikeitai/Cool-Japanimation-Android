ec918a1c3fceb5e44f451b796ec1d848bc992fe0
user2012 <user2012@2012-PC503>
2015-02-03 09:04:14 +0900

    last?

--- src/com/example/cj_app/AnimeSearch.java
+++ src/com/example/cj_app/AnimeSearch.java
@@ -1,10 +1,16 @@
 package com.example.cj_app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class AnimeSearch extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,41 @@ public class AnimeSearch extends ActionBarActivity {
 				startActivity(intent);
 				}
 		});
-
-
+		
+		// リソースに準備した画像ファイルからBitmapを作成しておく
+        Bitmap naruto, singeki, dragon;
+        naruto = BitmapFactory.decodeResource(getResources(), R.drawable.naruto_title);
+        singeki = BitmapFactory.decodeResource(getResources(), R.drawable.naruto_title);
+        dragon = BitmapFactory.decodeResource(getResources(), R.drawable.naruto_title);
+ 
+        // データの作成
+        List<CustomData> objects = new ArrayList<CustomData>();
+        CustomData item1 = new CustomData();
+        item1.setImagaData(naruto);
+        
+        CustomData item2 = new CustomData();
+        item2.setImagaData(singeki);
+        //item2.setTextData("The second");
+ 
+        CustomData item3 = new CustomData();
+        item3.setImagaData(dragon);
+        //item3.setTextData("Il terzo");
+        
+        CustomData item4 = new CustomData();
+        item4.setImagaData(dragon);
+        
+        CustomData item5 = new CustomData();
+        item4.setImagaData(naruto);
+        
+        objects.add(item1);
+        objects.add(item2);
+        objects.add(item3);
+        objects.add(item4);
+        objects.add(item5);
+ 
+        CustomAdapter customAdapater = new CustomAdapter(this, 0, objects);
+ 
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(customAdapater);
 	}
 }
